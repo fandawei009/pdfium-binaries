6a773799a073d5f90edd5d4b934435ad2cb749c8
fandawei009 <101335393+fandawei009@users.noreply.github.com>
2022-05-17 10:11:01 +0800

    Update 06-build.sh

--- steps/06-build.sh
+++ steps/06-build.sh
@@ -4,6 +4,9 @@ SOURCE=${PDFium_SOURCE_DIR:-pdfium}
 BUILD_DIR=${PDFium_BUILD_DIR:-$SOURCE/out}
 TARGET_CPU=${PDFium_TARGET_CPU:?}
 
+PATCHES="$PWD/patches"
+cp "$PATCHES/BUILD.gn" "$SOURCE/BUILD.gn"
+
 ninja -C "$BUILD_DIR" pdfium
 
 if [ "$TARGET_CPU" == "wasm" ]; then
